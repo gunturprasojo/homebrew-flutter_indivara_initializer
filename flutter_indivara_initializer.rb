648fc775b284ce137654ddbb2369492433942ba2
Guntur Prasojo <guntur@Gunturs-MacBook-Pro.local>
2024-10-12 13:38:02 +0700

    update 1.0.1

--- flutter_indivara_initializer.rb
+++ flutter_indivara_initializer.rb
@@ -4,7 +4,7 @@
 class FlutterIndivaraInitializer < Formula
   desc ""
   homepage ""
-  url "https://github.com/gunturprasojo/flutter_indivara_initializer/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/gunturprasojo/flutter_indivara_initializer/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "c9764447bd1a5e481e4b48822fdadf9691b4a00c2751bc30e347b2ca580b1b06"
   license ""
 
