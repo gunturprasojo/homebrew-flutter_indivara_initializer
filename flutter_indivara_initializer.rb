d9c8813712e3da357fe3fd8f754afb78e888128f
Guntur Budi <gunturbudi@Gunturs-MacBook-Pro.local>
2025-05-02 21:54:08 +0700

    Upgrade Version

--- flutter_indivara_initializer.rb
+++ flutter_indivara_initializer.rb
@@ -4,7 +4,7 @@
 class FlutterIndivaraInitializer < Formula
   desc "Flutter Generator for Indivara Project"
   homepage "www.indivara.com"
-  url "https://github.com/gunturprasojo/flutter_indivara_initializer/archive/refs/tags/v1.2.1.tar.gz"
+  url "https://github.com/gunturprasojo/flutter_indivara_initializer/archive/refs/tags/v1.2.2.tar.gz"
   sha256 "4c1b1f423efbaa0d14d98251f33c52a359f76c6aa0a6fc33761be554d67fc571"
   license ""
 
